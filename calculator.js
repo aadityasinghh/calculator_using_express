93bde9646bf8438dc6ce6338468a54e0f99c5991
ADITYA SINGH <adityasingh20102001@gmail.com>
2023-07-08 02:38:10 +0530

    final commit

--- calculator.js
+++ calculator.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
@@ -10,6 +11,17 @@ app.post("/", function (req, res) {
   var num2 = Number(req.body.num2);
   var result = num1 + num2;
   res.send("the sum of the two number is " + result);
+});
+
+app.get("/bmiCalculator", function (req, res) {
+  res.sendFile(__dirname + "/bmiCalculator.html");
+});
+
+app.post("/bmiCalculator", function (req, res) {
+  var weight = parseFloat(req.body.weight);
+  var height = parseFloat(req.body.height);
+  var bmi = weight / (height * height);
+  res.send("Your BMI is " + bmi);
 });
 
 app.listen(3000, function () {
