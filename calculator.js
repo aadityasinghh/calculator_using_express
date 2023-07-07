71d3ef06b3014d87e75ab0de96f195061e9eaf11
ADITYA SINGH <adityasingh20102001@gmail.com>
2023-07-08 02:12:44 +0530

    the next step fully fucntional calculator

--- calculator.js
+++ calculator.js
@@ -1,8 +1,15 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
-
+app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
+});
+app.post("/", function (req, res) {
+  var num1 = Number(req.body.num1); //the result which the body-parser gives is text and so we need to explicitly convert the text into number ,it doesnt matter what is the type of input in the form ,even if its number we need to that conversion
+  var num2 = Number(req.body.num2);
+  var result = num1 + num2;
+  res.send("the sum of the two number is " + result);
 });
 
 app.listen(3000, function () {
